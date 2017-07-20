56c4751f32ad461896793dd3dbd6a63f308356ec
jsuraj <jsurajbaramati@gmail.com>
2017-07-20 16:10:10 +0530

    added break statement in switch case, works fine, todo: handle empty input

--- src/components/views/taskList.js
+++ src/components/views/taskList.js
@@ -19,12 +19,31 @@ class TaskList extends React.Component {
   }
 
   getTodos(index) {
+    console.log('getTodos: '+index);
       var rows = []
       switch (index) {
         case 0:
+          console.log('case 0');
           for(var i=0;i<this.props.todos.length;i++) {
             rows.push(<Task todo={this.props.todos[i]} key={Math.random()} index={i} />)
           }
+          break;
+        case 1:
+          console.log('case 1');
+          for(var i=0;i<this.props.todos.length;i++) {
+            if(this.props.todos[i].completed === false) {
+              rows.push(<Task todo={this.props.todos[i]} key={Math.random()} index={i} />)
+            }
+          }
+          break;
+        case 2:
+          console.log('case 2');
+          for(var i=0;i<this.props.todos.length;i++) {
+            if(this.props.todos[i].completed === true) {
+              rows.push(<Task todo={this.props.todos[i]} key={Math.random()} index={i} />)
+            }
+          }
+          break;
       }
       return rows;
   }
@@ -33,7 +52,9 @@ class TaskList extends React.Component {
     var rows = [];
     var buttons = '';
 
-    rows = this.getTodos(0);
+    rows = this.getTodos(this.props.active);
+    console.log('rows: '+rows);
+
     if(this.props.todos.length>0) {
       buttons = this.getButtons();
 
